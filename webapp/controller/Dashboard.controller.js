1d9c8d7a7309c25d17b54cc513a49c616c178b75
jpoornimaprasanna <codingpoornima@gmail.com>
2020-10-01 10:21:37 +0000

    table data

--- webapp/controller/Dashboard.controller.js
+++ webapp/controller/Dashboard.controller.js
@@ -1,26 +1,54 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"ns/UserDetails/formatters/activeusers",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/ui/core/format/DateFormat"
+], function (Controller, formatter, Filter, FilterOperator, DateFormat) {
 	"use strict";
 
 	return Controller.extend("ns.UserDetails.controller.Dashboard", {
+		formatter: formatter,
+
 		onInit: function () {
 			this.getCount();
 		},
 
 		getCount: function () {
-			var ODataModel = "sap/opu/odata/sap/ZUSERDATA_SRV";
-			this._model = new sap.ui.model.odata.ODataModel(ODataModel, true);
-			this.getView().setModel(this._model);
-			debugger;
-			this.getView().getModel().read("/totalUsersSet/$count", {
+			var ODataModel = "/sap/opu/odata/sap/ZUSERDATA_SRV";
+			var oModel = new sap.ui.model.odata.ODataModel(ODataModel, true);
+			this.getView().setModel(oModel);
+
+			oModel.read("/totalUsersSet", {
 				success: function (oResult) {
 					// check if the result is a number
-					if (isNaN(oResult)) {
-						var oIntResult = parseInt(oResult);
-					}
-				}
-			}); 
+
+					var totalUsers = oResult.results.length;
+					// var model = this.getView().getModel("userModel")
+					this.getView().getModel("userModel").setProperty("/totalUsers", totalUsers);
+					this.getView().getModel("userModel").setProperty("/activeUser", oResult.results);
+					// if (isNaN(oResult)) {
+					// 	var oIntResult = parseInt(oResult);
+					// }
+				}.bind(this)
+			});
+		},
+		onSearch: function (oEvent) {
+
+			var olist = this.getView().byId("idProductsTable"),
+				// this.getView().getModel("userModel").getProperty("username");
+				arr = [],
+				binding,
+				filters;
+			filters = new Filter({
+				filters: [new Filter("Bname", FilterOperator.Contains, oEvent.getSource().getValue())
+
+				],
+				and: false
+			});
+			binding = olist.getBinding("items");
+			arr.push(filters);
+			binding.filter(arr);
 		}
 	});
 });
